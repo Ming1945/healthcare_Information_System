9879f1dd25f9dc3bc1c71814858d3677721a1740
Ming1945 <ming.fc45yahoo.com>
2023-11-07 19:31:06 +0100

    ver 1.0.2 : homepage - login container - close function

--- frontend/src/app/components/homepage/homepage.component.ts
+++ frontend/src/app/components/homepage/homepage.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+
+  close = false;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  showLogin() {
+    this.close = !this.close;
+  }
 
 }
